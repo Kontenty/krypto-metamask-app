761c00cd5c5819433a38b54890b3a0e54f3e3708
Konard Górski <konradgrsk@gmail.com>
2022-01-04 22:25:16 +0100

    define deploy script and hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
+//metamask https://eth-ropsten.alchemyapi.io/v2/UMamTclmzHv5G6g-xV0RspTv5XbgsdyF
+
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/UMamTclmzHv5G6g-xV0RspTv5XbgsdyF",
+      accounts: [ "f89e6ec32cbdb9cb41b1dc4b8aa0d48cee5b8d308b4151029241a41c04a271a5" ],
+    },
+  },
 };
