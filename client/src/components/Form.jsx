fe43f14ca17990e04d885e1adb587c8a417d761d
Konard Górski <konradgrsk@gmail.com>
2022-01-04 17:38:45 +0100

    form handling with react hook form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const inputs = [
   { name: 'address', placeholder: 'Address To' },
-  { name: 'amount', placeholder: 'Amount (ETH)' },
+  { name: 'amount', placeholder: 'Amount (ETH)', type: 'number' },
   { name: 'keyword', placeholder: 'Keyword (gif)' },
   { name: 'twitter', placeholder: 'Twitter @' },
 ];
 
-const Form = () => (
-  <form className="flex flex-col p-10 blue-glassmorphism">
-    {inputs.map((input) => (
-      <input
-        type="text"
-        name={input.name}
-        key={input.name}
-        placeholder={input.placeholder}
-        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-small white-glassmorphism"
-      />
-    ))}
-    <textarea
-      name="message"
-      id="message"
-      placeholder="Enter message"
-      className="mt-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-small white-glassmorphism resize-none"
-    />
-    <hr className="border-gray-400 my-4" />
-    <button
-      type="submit"
-      className="text-white border-gray-400 border-2 rounded-full py-2"
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col p-10 blue-glassmorphism"
     >
-      Send now
-    </button>
-  </form>
-);
+      {inputs.map((input) => (
+        <input
+          type={input.type ?? 'text'}
+          key={input.name}
+          placeholder={input.placeholder}
+          {...register(input.name)}
+          className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-small white-glassmorphism"
+        />
+      ))}
+      <textarea
+        name="message"
+        id="message"
+        placeholder="Enter message"
+        className="mt-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-small white-glassmorphism resize-none"
+      />
+      <hr className="border-gray-400 my-4" />
+      <button
+        type="submit"
+        className="text-white border-gray-400 border-2 rounded-full py-2"
+      >
+        Send now
+      </button>
+    </form>
+  );
+};
 
 export default Form;
