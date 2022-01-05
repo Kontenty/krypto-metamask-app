49c4abab675d5ac2f577753c20118d44d6bf2ea1
Konard Górski <konradgrsk@gmail.com>
2022-01-05 13:25:50 +0100

    ability to connect wallet by click

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
+import { TransactionContext } from '../context/transactionContext';
 import Loader from './Loader';
 import Form from './Form';
 
@@ -10,7 +11,7 @@ const commonstyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w=[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white';
 
 const Welcome = () => {
-  const connectWallet = () => {};
+  const { connectWallet, currentAccount } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center">
@@ -22,19 +23,25 @@ const Welcome = () => {
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies on Krypto
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="btn-blue rounded-full py-3 mt-3"
-          >
-            Connect wallet
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="btn-blue rounded-full py-3 mt-3"
+            >
+              Connect wallet
+            </button>
+          )}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonstyles}`}>Reliability</div>
-            <div className={`rounded-tr-2xl sm:rounded-none ${commonstyles}`}>Security</div>
+            <div className={`rounded-tr-2xl sm:rounded-none ${commonstyles}`}>
+              Security
+            </div>
             <div className={`sm:rounded-tr-2xl ${commonstyles}`}>Ethereum</div>
             <div className={`sm:rounded-bl-2xl ${commonstyles}`}>Web 3.0</div>
-            <div className={`rounded-bl-2xl sm:rounded-none ${commonstyles}`}>Low fees</div>
+            <div className={`rounded-bl-2xl sm:rounded-none ${commonstyles}`}>
+              Low fees
+            </div>
             <div className={`rounded-br-2xl ${commonstyles}`}>Blockchain</div>
           </div>
         </article>
